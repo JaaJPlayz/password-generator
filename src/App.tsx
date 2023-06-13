9f47d2f9c4599d3419a8d3cb65e75e8d771cf7f8
JaaJPlayz <hiagoaugustofcunha@gmail.com>
2023-06-12 21:02:18 -0300

    End of the challenge

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [passSize, setPassSize] = useState("");
+  const [generatedPassword, setGeneratedPassword] = useState("");
+
+  const passwordGenerator = (size: number) => {
+    let password = "";
+    const possible =
+      'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*-_=~`|/:;"<>()?';
+
+    for (let i = 0; i < size; i++) {
+      password += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+
+    return password;
+  };
+
+  const handleGeneratePassword = () => {
+    const size = parseInt(passSize);
+    const password = passwordGenerator(size);
+    setGeneratedPassword(password);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="App">
+        <header>
+          <h1>🔒Gerador de Senhas Aleatórias</h1>
+        </header>
+        <label><h2>Escolha o tamanho da senha:</h2></label>
+        <input
+          type="number"
+          placeholder="Tamanho da senha"
+          value={passSize}
+          onChange={(e) => setPassSize(e.target.value)}
+        />
+        <button onClick={handleGeneratePassword}>Gerar</button>
+        <h3>Senha Gerada:</h3>
+        <h4>{generatedPassword}</h4>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
